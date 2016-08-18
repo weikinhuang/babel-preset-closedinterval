b2d7ec38d75f201ff8b2d5ce821542df7a54e799
Wei Kin Huang <weikin.huang04@gmail.com>
2016-08-17 20:58:09 -0400

    v1.0.0

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets : [
+    [ require('babel-preset-es2015'), { modules: false } ],
+    require('babel-preset-es2016'),
+    require('babel-preset-stage-3'),
+    require('babel-preset-stage-2'),
+  ],
+  plugins : [
+    require('babel-plugin-transform-es5-property-mutators'),
+    require('babel-plugin-transform-node-env-inline'),
+  ],
+  env : {
+    production : {
+      plugins : [
+        require('babel-plugin-transform-inline-environment-variables')
+      ]
+    },
+    test : {
+      plugins : [
+        require('babel-plugin-istanbul')
+      ]
+    }
+  }
+};
