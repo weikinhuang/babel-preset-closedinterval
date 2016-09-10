5a6b078a8e35b75fbe8ed1e36854da1f06ea3885
Wei Kin Huang <weikin.huang04@gmail.com>
2016-09-09 21:24:57 -0400

    Add babel-plugin-rewire and allow node mode in options

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-module.exports = {
-  presets : [
-    [ 'babel-preset-es2015', { modules: false } ],
-    require('babel-preset-es2016'),
-    require('babel-preset-es2017'),
-    require('babel-preset-stage-3'),
-    require('babel-preset-stage-2'),
-  ],
-  plugins : [
-    require('babel-plugin-transform-es5-property-mutators'),
-    require('babel-plugin-transform-node-env-inline'),
-  ],
-  env : {
-    production : {
-      plugins : [
-        require('babel-plugin-transform-inline-environment-variables')
-      ]
-    },
-    test : {
-      plugins : [
-        require('babel-plugin-istanbul')
-      ]
+const es2015BuildPreset = require('babel-preset-es2015').buildPreset;
+
+module.exports = function preset(context, opts) {
+  opts = opts || {};
+
+  return {
+    presets: [
+      [es2015BuildPreset, { modules: opts.node ? 'commonjs' : false }],
+      require('babel-preset-es2016'),
+      require('babel-preset-es2017'),
+      require('babel-preset-stage-3'),
+      require('babel-preset-stage-2'),
+    ],
+    plugins: [
+      require('babel-plugin-transform-es5-property-mutators'),
+      require('babel-plugin-transform-node-env-inline'),
+    ],
+    env: {
+      production: {
+        plugins: [
+          require('babel-plugin-transform-inline-environment-variables')
+        ]
+      },
+      test: {
+        plugins: [
+          require('babel-plugin-istanbul'),
+          require('babel-plugin-rewire')
+        ]
+      }
     }
-  }
+  };
 };
